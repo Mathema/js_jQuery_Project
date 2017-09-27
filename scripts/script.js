7bb83529e6a1d366a3a90fc5d5cdd86637d48686
Yavuz <yavuz.orbey@gmail.com>
2017-09-27 17:28:16 -0400

    Added the option to have a custom grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
 
-  for(var i=0; i<16*16; i++){
-    var block = $('<div class="block"></div>');
-    $('#grid').append(block);
-  }
-  $('.block').on('mouseenter', function(){
-    $(this).addClass('highlight');
-  });
+  function setUp(){
+    $('.block').remove();
+    var numberOfBlocks=prompt("How many squares per side?");
+    for(var i=0; i<numberOfBlocks*numberOfBlocks; i++){
+      var block = $('<div class="block"></div>');
+      $('#grid').append(block);
+    }
+    $('.block').css({'height': 864/numberOfBlocks+'px', 'width': 960/numberOfBlocks+'px'});
+    $('.block').on('mouseenter', function(){
+      $(this).addClass('highlight');
+    });
+  };
+
   $('.reset').on('click', function(){
     $('.block').removeClass('highlight');
-  })
+    setUp();
+
+  });
 });
