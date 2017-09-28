c6d9f04f191e72d54b1854b01014f71b32844e5e
Yavuz <yavuz.orbey@gmail.com>
2017-09-28 12:20:53 -0400

    Fixed stability issues and added more color options

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,48 @@
 $(document).ready(function(){
+  var currentcolor = '#000000';
+  var colorcodes = ['#FF0000', '#FF7C00', '#FFFF00', '#5DFF00', '#0078FF', '#B200FF'];
 
   function setUp(){
     $('.block').remove();
     var numberOfBlocks=prompt("How many squares per side?");
+    alert(numberOfBlocks);
+    if(!numberOfBlocks)
+    numberOfBlocks = 1;
     for(var i=0; i<numberOfBlocks*numberOfBlocks; i++){
       var block = $('<div class="block"></div>');
       $('#grid').append(block);
     }
     $('.block').css({'height': 864/numberOfBlocks+'px', 'width': 960/numberOfBlocks+'px'});
+
     $('.block').on('mouseenter', function(){
-      $(this).addClass('highlight');
+
+      //$(this).addClass(currentcolor);
+      $(this).css('background-color', currentcolor);
     });
   };
 
-  $('.reset').on('click', function(){
-    $('.block').removeClass('highlight');
-    setUp();
 
+
+  $('.reset').on('click', function(){
+    //$('.block').removeClass('highlight');
+
+    setUp();
   });
+
+  $('.random').on('click', function(){
+    //$('.block').removeClass(currentcolor);
+    currentcolor = colorcodes[Math.floor(Math.random()*colorcodes.length)];
+  });
+  $('.rainbow').on('click', function(){
+    //$('.block').removeClass(currentcolor);
+    var x =0;
+    $('.block').on('mouseenter', function(){
+      x%=colorcodes.length;
+       currentcolor = colorcodes[x];
+      //$(this).addClass(currentcolor);
+      $(this).css('background-color', currentcolor);
+      x++;
+    });
+  });
+
 });
