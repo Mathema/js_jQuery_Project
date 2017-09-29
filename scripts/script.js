735e9ff06649dd9cc7f68ad2599d8cc39d1ac88e
Yavuz <yavuz.orbey@gmail.com>
2017-09-28 20:19:31 -0400

    More styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,67 @@
 $(document).ready(function(){
   var currentcolor = '#000000';
   var colorcodes = ['#FF0000', '#FF7C00', '#FFFF00', '#5DFF00', '#0078FF', '#B200FF'];
-
+  var state ='';
   function setUp(){
     $('.block').remove();
-    var numberOfBlocks=prompt("How many squares per side?");
-    alert(numberOfBlocks);
+    var numberOfBlocks=prompt("How many squares per side?\n(For best performance choose a number below 150)");
+
     if(!numberOfBlocks)
     numberOfBlocks = 1;
+
     for(var i=0; i<numberOfBlocks*numberOfBlocks; i++){
       var block = $('<div class="block"></div>');
       $('#grid').append(block);
     }
-    $('.block').css({'height': 864/numberOfBlocks+'px', 'width': 960/numberOfBlocks+'px'});
+    $('.block').css({'height': $('#grid').height()/numberOfBlocks+'px', 'width': 960/numberOfBlocks+'px'});
 
+    //draw();
+
+  };
+
+  function draw(){
+    var x=0;
     $('.block').on('mouseenter', function(){
 
       //$(this).addClass(currentcolor);
+      switch(state){
+        case 'rainbow':
+          x%=colorcodes.length;
+          currentcolor = colorcodes[x];
+          x++;
+          break;
+        default:
+          currentcolor = currentcolor;
+          break;
+
+      }
       $(this).css('background-color', currentcolor);
     });
   };
 
-
-
   $('.reset').on('click', function(){
     //$('.block').removeClass('highlight');
-
     setUp();
+    draw();
   });
 
   $('.random').on('click', function(){
     //$('.block').removeClass(currentcolor);
+    state = 'random';
     currentcolor = colorcodes[Math.floor(Math.random()*colorcodes.length)];
+    //currentcolor = colorcodes[Math.floor(Math.random()*colorcodes.length)];
+    //draw();
   });
   $('.rainbow').on('click', function(){
+    state = 'rainbow';
+    ;
     //$('.block').removeClass(currentcolor);
-    var x =0;
-    $('.block').on('mouseenter', function(){
-      x%=colorcodes.length;
-       currentcolor = colorcodes[x];
+    //var x =0;
+    //$('.block').on('mouseenter', function(){
+      //x%=colorcodes.length;
+       //currentcolor = colorcodes[x];
       //$(this).addClass(currentcolor);
-      $(this).css('background-color', currentcolor);
-      x++;
+      //$(this).css('background-color', currentcolor);
+//x++;
     });
   });
-
-});
